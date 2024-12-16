1ce1fd563d8d099deec79e4e5ae9534b419d81c9
lang-sudo <21220007@respati.ac.id>
2024-12-16 03:18:46 +0000

    Hitung Keliling dan Luas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Hitung Keliling dan Luas")
+st.write("Deskripsi")
+
+panjang = st.number_input("Masukkan Panjang Persegi", value=0)
+st.write("Panjang persegi: ", panjang)
+
+lebar = st.number_input("Masukkan Lebar Persegi", value=0)
+st.write("Lebar persegi: ", lebar)
+
+if (lebar & panjang):
+    st.subheader('keliling')
+    st.write( 2 * ( panjang + lebar ) )
+
+    st.subheader('Luas')
+    st.write( panjang * lebar )
